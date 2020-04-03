e5466f2b47efc735f06b91ce1989520b19aa0f56
Twan Goosen <twan@clarin.eu>
2020-04-03 11:10:57 +0200

    Update txt-to-qr.sh

--- txt-to-qr.sh
+++ txt-to-qr.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# https://github.com/twagoo/text-to-qr-script
+#
+# Requires qrencode. To install (on MacOS): `brew install qrencode`
+
 OF=$(mktemp).png;
 qrencode -o ${OF} $(read -s a && echo $a && unset a)
 open $OF
